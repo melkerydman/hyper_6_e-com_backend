4fa02cb3e66031c6abea30dbe7cb502f1d7f0e08
melkerydman <melkerrydman@gmail.com>
2022-03-31 11:16:59 +0200

    updated handler to update cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -9,37 +9,13 @@ import { Db, ObjectId } from "mongodb";
 // Add new cart document to collection
 const addCartHandler = (db) => async (req, res) => {
   const cart = req.body;
+  console.log("cart: ", cart);
 
   const result = await db.collection("cart").insertOne(cart);
 
   res.json({
     id: result.insertedId,
   });
-};
-
-// If cart document exists – update it
-const updateCartHandler = (db) => async (req, res) => {
-  const cartId = req.params.cartId;
-  const requestBody = req.body;
-
-  if (!ObjectId.isValid(cartId)) {
-    res.sendStatus(404);
-    return;
-  }
-
-  const documentCount = await db.collection("cart").count({
-    _id: new ObjectId(cartId),
-  });
-  const cartExists = documentCount === 1;
-
-  if (cartExists) {
-    await db
-      .collection("cart")
-      .updateOne({ _id: new ObjectId(cartId) }, { $set: requestBody });
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(404);
-  }
 };
 
 // Find existing cart document
@@ -52,6 +28,39 @@ const findCartByIdHandler = (db) => async (req, res) => {
 
   if (cart) {
     res.json(cart);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+// If cart document exists – update it
+const updateCartHandler = (db) => async (req, res) => {
+  const cartId = req.params.cartId;
+  const requestBody = req.body;
+  console.log("requestBody: ", requestBody);
+
+  if (!ObjectId.isValid(cartId)) {
+    res.sendStatus(404);
+    return;
+  }
+
+  const documentCount = await db.collection("cart").count({
+    _id: new ObjectId(cartId),
+  });
+  const cartExists = documentCount === 1;
+
+  if (cartExists) {
+    await db.collection("cart").updateOne(
+      { _id: new ObjectId(cartId) },
+      {
+        $set: {
+          isShowing: requestBody.cart.isShowing,
+          items: requestBody.cart.items,
+          totalQuantity: requestBody.cart.totalQuantity,
+        },
+      }
+    );
+    res.sendStatus(200);
   } else {
     res.sendStatus(404);
   }
