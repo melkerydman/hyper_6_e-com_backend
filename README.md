2abffe5190b2902c3244bd51450b831dbdf64ff8
melkerydman <melkerrydman@gmail.com>
2022-05-04 15:32:11 +0200

    Update to cart routes

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ### To run MongoDB
 
 brew services start mongodb/brew/mongodb-community
+mongosh
+
+### Tech Stack
+
+MongoDB
+Express
+Cors - for handling Cors errors
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -6,12 +6,17 @@ import { Db, ObjectId } from "mongodb";
  * Handlers take a MongoDB instance and returns an Express handler function to be registered on the router at the bottom
  */
 
+// Pass cartId
+// If cart id exists, update cart
+// If id doesn't exist, create with upsert id
+
 // Find existing cart document
 const getCartById = (db) => async (req, res) => {
   const cartId = req.params.cartId;
+  console.log(cartId);
   const collection = await db.collection("carts");
 
-  const cart = await collection.findOne({ _id: cartId });
+  const cart = await collection.findOne({ _id: new ObjectId(cartId) });
 
   if (cart) {
     res.json(cart);
@@ -22,12 +27,15 @@ const getCartById = (db) => async (req, res) => {
 };
 
 const addToCart = (db) => async (req, res) => {
-  const { userId, productId, quantity, title, price } = req.body;
+  const { cartId, productId, quantity, title, price } = req.body;
+
+  // console.log("passed data: ", req.body);
+  console.log("cartId: ", cartId);
 
   const collection = await db.collection("carts");
 
   const inCart = await collection.count({
-    _id: userId,
+    _id: new ObjectId(cartId),
     "items._id": new ObjectId(productId),
   });
 
@@ -36,10 +44,10 @@ const addToCart = (db) => async (req, res) => {
 
   if (inCart > 0) {
     console.log(
-      "Aldready in cart, will update item and increase total quantity"
+      "Already in cart, will update item and increase total quantity"
     );
     await collection.updateOne(
-      { _id: userId, "items._id": new ObjectId(productId) },
+      { _id: new ObjectId(cartId), "items._id": new ObjectId(productId) },
       {
         $inc: {
           "items.$.quantity": quantity,
@@ -48,14 +56,15 @@ const addToCart = (db) => async (req, res) => {
         },
       }
     );
-    res.status(200).json(userId);
+
+    res.status(200).json(cartId);
   }
 
   // Document not exists then add document
   else {
     console.log("Not in cart, will create and update total quantity");
-    await collection.updateOne(
-      { _id: userId },
+    const newCart = await collection.updateOne(
+      { _id: cartId !== undefined ? new ObjectId(cartId) : new ObjectId() },
       {
         $inc: {
           totalQuantity: quantity,
@@ -72,25 +81,28 @@ const addToCart = (db) => async (req, res) => {
       },
       { upsert: true }
     );
-    res.status(200).json(userId);
+    console.log("newCart: ", newCart);
+    res.status(200).json(cartId !== undefined ? cartId : newCart);
   }
 };
 
 const removeFromCart = (db) => async (req, res) => {
-  const { userId, productId, clear } = req.body;
+  const { cartId, productId, clear } = req.body;
+  console.log(req.body);
 
   const collection = await db.collection("carts");
 
-  const cart = await collection.findOne({ _id: userId });
+  const cart = await collection.findOne({ _id: new ObjectId(cartId) });
   const cartItemToRemove = cart.items.find(
     (item) => item._id.toString() === new ObjectId(productId).toString()
   );
+  console.log("item to remove:", cartItemToRemove);
 
   if (!clear) {
     if (cartItemToRemove.quantity > 1) {
       console.log("mulitple items left");
       await collection.updateOne(
-        { _id: userId, "items._id": new ObjectId(productId) },
+        { _id: new ObjectId(cartId), "items._id": new ObjectId(productId) },
         {
           $inc: {
             "items.$.quantity": -1,
@@ -103,7 +115,7 @@ const removeFromCart = (db) => async (req, res) => {
     // else {
     //   console.log("only one item left");
     //   await collection.updateOne(
-    //     { _id: userId, "items._id": new ObjectId(productId) },
+    //     { _id: cartId, "items._id": new ObjectId(productId) },
     //     {
     //       $inc: {
     //         totalPrice: -cartItemToRemove.price,
@@ -113,12 +125,12 @@ const removeFromCart = (db) => async (req, res) => {
     //     }
     //   );
     // }
-    res.status(200).json(userId);
+    res.status(200).json(cartId);
   } else {
     const sumToSubract = cartItemToRemove.price * cartItemToRemove.quantity;
 
     await collection.updateOne(
-      { _id: userId, "items._id": new ObjectId(productId) },
+      { _id: new ObjectId(cartId), "items._id": new ObjectId(productId) },
       {
         $inc: {
           totalPrice: -sumToSubract,
@@ -127,7 +139,7 @@ const removeFromCart = (db) => async (req, res) => {
         $pull: { items: { _id: new ObjectId(productId) } },
       }
     );
-    res.status(200).json(userId);
+    res.status(200).json(cartId);
   }
 };
 
