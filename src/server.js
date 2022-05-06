ead9d7a5a746198c39d10a37aee02a5628a331c8
melkerydman <melkerrydman@gmail.com>
2022-05-06 08:04:52 +0200

    Debugging CORS error

--- src/server.js
+++ src/server.js
@@ -11,11 +11,19 @@ import { cartRoutes } from "./routes/cart.js";
  * @param db {Db} A connected MongoDB instance
  * @returns {Express}
  */
+
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+// };
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://hyper-6-ecom.netlify.app/"],
+  credentials: true,
+};
 export function createServer(db) {
   const app = express();
 
   app.use(express.json());
-  app.use(cors());
+  app.use(cors(corsOptions));
   app.use("/products", productRoutes(db));
   app.use("/orders", orderRoutes(db));
   app.use("/cart", cartRoutes(db));
